699d53331f5177bd9c7112bb5bb7f8be067507f7
Vane <Vane@vanedeiMac.local>
2018-01-19 12:21:54 +0800

    add files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # find-big-files
 find big files from current directory
+## Overview
+This tool will ordered current directory's file by size. File statistics will output to file "find_big_files.txt".
+## Prerequisites
+* ruby
+* mac
+## Usage
+all files in current directory 
+```
+ruby find_big_files.rb
+```
+files with suffix
+```
+ruby find_big_files.rb png
+```
+files with multi suffix
+```
+ruby find_big_files.rb png,jpg,webp
+```
+
---
+++ find_big_files.rb
@@ -0,0 +1,26 @@
+custom_suffix = ""
+ARGV.each { |a|
+  custom_suffix = a
+  break
+}
+
+allfiles = nil
+if custom_suffix.empty?
+  allfiles = Dir["**/*.*"]
+else
+  allfiles = Dir["**/*.{#{custom_suffix}}"]
+end
+allfiles = allfiles.sort_by {|filename| File.size(filename) }
+allfiles.reverse!
+
+file_target_name = "find_big_files.txt"
+file_target = File.new(file_target_name,  "w+")
+allfiles.each { |file|
+    filename = File.basename(file)
+    file_size = '%.3f' % (File.size(file).to_f / 2**20)
+    file_target.puts "#{file_size}MB  #{filename}  #{file}"
+}
+
+puts "********************** Files Statistics: #{file_target_name}"
+puts "********************** Files count: #{allfiles.count}"
+puts "********************** Completed!"
